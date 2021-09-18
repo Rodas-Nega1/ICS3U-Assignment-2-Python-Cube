264e175493dc518aad45351516623f87cce1432d
Ubuntu <rodas.nega@mths.ca>
2021-09-18 22:34:47 +0000

    cube math program

---
+++ cube.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by: Rodas Nega
+# Created on: Sep 2021
+# This program calculates the surface and area of a cube
+#    with side length inputted from the user
+
+
+def main():
+    # this function calculates the surface area and volume of a cube
+
+    # input
+    side_length = int(
+        input("Enter length of the side length of your cube (cm): ")
+    )
+
+    # process
+    surface_area = 6 * side_length ** 2
+    volume = side_length ** 3
+
+    # output
+    print("")
+    print("The surface area of the cube is {0} cm².".format(surface_area))
+    print("The volume of the cube is {0} cm³.".format(volume))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
